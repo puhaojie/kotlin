a163c0cf47ed2e18c2e2e3fc70cdf32982e013f2
puhaojie <1293753897@qq.com>
2018-05-30 14:26:15 +0800

    完成购物车业务逻辑

--- KotlinMall/GoodsCenter/src/main/java/com/kotlin/goods/ui/fragment/CartFragment.kt
+++ KotlinMall/GoodsCenter/src/main/java/com/kotlin/goods/ui/fragment/CartFragment.kt
@@ -1,24 +1,48 @@
 package com.kotlin.goods.ui.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.alibaba.android.arouter.launcher.ARouter
+import com.eightbitlab.rxbus.Bus
+import com.eightbitlab.rxbus.registerInBus
+import com.kennyc.view.MultiStateView
+import com.kotlin.base.ext.onClick
 import com.kotlin.base.ext.setVisible
+import com.kotlin.base.ext.startLoading
 import com.kotlin.base.ui.fragment.BaseMvpFragment
+import com.kotlin.base.utils.AppPrefsUtils
+import com.kotlin.base.utils.YuanFenConverter
 import com.kotlin.goods.R
+import com.kotlin.goods.common.GoodsConstant
 import com.kotlin.goods.data.protocol.CartGoods
+import com.kotlin.goods.event.CartAllCheckedEvent
+import com.kotlin.goods.event.UpdateCartSizeEvent
+import com.kotlin.goods.event.UpdateTotalPriceEvent
 import com.kotlin.goods.injection.component.DaggerCartComponent
 import com.kotlin.goods.injection.module.CartModule
 import com.kotlin.goods.presenter.CartListPresenter
 import com.kotlin.goods.presenter.view.CartListView
+import com.kotlin.goods.ui.adapter.CartGoodsAdapter
+import com.kotlin.provider.common.ProviderConstant
+import com.kotlin.provider.router.RouterPath
 import kotlinx.android.synthetic.main.fragment_cart.*
+import org.jetbrains.anko.support.v4.toast
+import rx.Subscriber
+import rx.functions.Action1
 
 /**
  * create by phj at 2018-05-30
  * 简介：购物车界面
  */
 class CartFragment : BaseMvpFragment<CartListPresenter>(), CartListView {
+
+    private lateinit var mAdapter: CartGoodsAdapter
+
+    private var mTotalPrice: Long = 0
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
@@ -33,17 +57,101 @@ class CartFragment : BaseMvpFragment<CartListPresenter>(), CartListView {
     }
 
     /**
+     * 加载数据
+     */
+    override fun onStart() {
+        super.onStart()
+        loadData()
+    }
+
+    /**
      *  初始化界面
      */
-    private fun initView(){
+    private fun initView() {
+        mCartGoodsRv.layoutManager = LinearLayoutManager(context)
+        mAdapter = CartGoodsAdapter(context!!)
+        mCartGoodsRv.adapter = mAdapter
 
+        mHeaderBar.getRightView().onClick {
+            refreshEditStatus()
+        }
+
+        //全选按钮事件
+        mAllCheckedCb.onClick {
+            for (item in mAdapter.dataList) {
+                item.isSelected = mAllCheckedCb.isChecked
+            }
+            mAdapter.notifyDataSetChanged()
+            updateTotalPrice()
+        }
+
+        //删除按钮事件
+        mDeleteBtn.onClick {
+            val cartIdList: MutableList<Int> = arrayListOf()
+            mAdapter.dataList.filter { it.isSelected }
+                    .mapTo(cartIdList) { it.id }
+            if (cartIdList.size == 0) {
+                toast("请选择需要删除的数据")
+            } else {
+                mPresenter.deleteCartList(cartIdList)
+            }
+        }
+
+        //结算按钮事件
+        mSettleAccountsBtn.onClick {
+            val cartGoodsList: MutableList<CartGoods> = arrayListOf()
+            mAdapter.dataList.filter { it.isSelected }
+                    .mapTo(cartGoodsList) { it }
+            if (cartGoodsList.size == 0) {
+                toast("请选择需要提交的数据")
+            } else {
+                mPresenter.submitCart(cartGoodsList, mTotalPrice)
+            }
+        }
+    }
+
+    /**
+     * 刷新是否为编辑状态
+     */
+    private fun refreshEditStatus() {
+        val isEditStatus = getString(R.string.common_edit) == mHeaderBar.getRightText()
+        mTotalPriceTv.setVisible(isEditStatus.not())
+        mSettleAccountsBtn.setVisible(isEditStatus.not())
+        mDeleteBtn.setVisible(isEditStatus)
+
+        mHeaderBar.getRightView().text = if (isEditStatus) getString(R.string.common_complete) else getString(R.string.common_edit)
     }
 
     /**
      * RxBus观察者注册
      */
-    private fun initObserve(){
+    private fun initObserve() {
+        Bus.observe<CartAllCheckedEvent>().subscribe {
+            t ->
+            run {
+                mAllCheckedCb.isChecked = t.isAllChecked
+                updateTotalPrice()
+            }
+        }
+                .registerInBus(this)
 
+        Bus.observe<UpdateTotalPriceEvent>().subscribe {
+            updateTotalPrice()
+        }
+                .registerInBus(this)
+    }
+
+    /**
+     * 更新总价
+     */
+    @SuppressLint("SetTextI18n")
+    private fun updateTotalPrice() {
+        mTotalPrice = mAdapter.dataList
+                .filter { it.isSelected }
+                .map { it.goodsCount * it.goodsPrice }
+                .sum()
+
+        mTotalPriceTv.text = "合计:${YuanFenConverter.changeF2YWithUnit(mTotalPrice)}"
     }
 
     override fun injectComponent() {
@@ -52,20 +160,48 @@ class CartFragment : BaseMvpFragment<CartListPresenter>(), CartListView {
     }
 
     override fun onGetCartListResult(result: MutableList<CartGoods>?) {
+        if (result != null && result.size > 0) {
+            mAdapter.setData(result)
+            mHeaderBar.getRightView().setVisible(true)
+            mAllCheckedCb.isChecked = false
+            mMultiStateView.viewState = MultiStateView.VIEW_STATE_CONTENT
+        } else {
+            mHeaderBar.getRightView().setVisible(false)
+            mMultiStateView.viewState = MultiStateView.VIEW_STATE_EMPTY
+        }
 
+        //本地存储并发送事件刷新UI
+        AppPrefsUtils.putInt(GoodsConstant.SP_CART_SIZE, result?.size ?: 0)
+        Bus.send(UpdateCartSizeEvent())
+        //更新总价
+        updateTotalPrice()
     }
 
     override fun onDeleteCartListResult(result: Boolean) {
+        toast("删除成功")
+        refreshEditStatus()
+        loadData()
     }
 
     override fun onSubmitCartListResult(result: Int) {
+        ARouter.getInstance().build(RouterPath.OrderCenter.PATH_ORDER_CONFIRM)
+                .withInt(ProviderConstant.KEY_ORDER_ID, result)
+                .navigation()
+    }
+
+    /*
+        加载数据
+    */
+    private fun loadData() {
+        mMultiStateView.startLoading()
+        mPresenter.getCartList()
     }
 
 
     /*
         设置Back是否可见
     */
-    fun setBackVisible(isVisible:Boolean){
+    fun setBackVisible(isVisible: Boolean) {
         mHeaderBar.getLeftView().setVisible(isVisible)
     }
 }
