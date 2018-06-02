8a376315289a2c1bf2f5510139b798a84f8c8156
puhaojie <1293753897@qq.com>
2018-06-02 15:49:40 +0800

    增加订单详情支付等显示

--- KotlinMall/OrderCenter/src/main/java/com/kotlin/order/injection/component/OrderComponent.kt
+++ KotlinMall/OrderCenter/src/main/java/com/kotlin/order/injection/component/OrderComponent.kt
@@ -4,6 +4,8 @@ import com.kotlin.base.injection.PerComponentScope
 import com.kotlin.base.injection.component.ActivityComponent
 import com.kotlin.order.injection.module.OrderModule
 import com.kotlin.order.ui.activity.OrderConfirmActivity
+import com.kotlin.order.ui.activity.OrderDetailActivity
+import com.kotlin.order.ui.fragment.OrderFragment
 import dagger.Component
 
 /*
@@ -13,7 +15,7 @@ import dagger.Component
 @Component(dependencies = arrayOf(ActivityComponent::class),modules = arrayOf(OrderModule::class))
 interface OrderComponent {
     fun inject(activity:OrderConfirmActivity)
-//    fun inject(fragment:OrderFragment)
-//
-//    fun inject(activity:OrderDetailActivity)
+    fun inject(fragment: OrderFragment)
+
+    fun inject(activity: OrderDetailActivity)
 }
--- KotlinMall/OrderCenter/src/main/java/com/kotlin/order/ui/adapter/OrderVpAdapter.kt
+++ KotlinMall/OrderCenter/src/main/java/com/kotlin/order/ui/adapter/OrderVpAdapter.kt
@@ -6,7 +6,7 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import com.kotlin.order.common.OrderConstant
-//import com.kotlin.order.ui.fragment.OrderFragment
+import com.kotlin.order.ui.fragment.OrderFragment
 
 /*
     订单Tab对应ViewPager
@@ -17,7 +17,7 @@ class OrderVpAdapter(fm: FragmentManager, context: Context)
     private val titles = arrayOf("全部","待付款","待收货","已完成","已取消")
 
     override fun getItem(position: Int): Fragment {
-        val fragment = Fragment()
+        val fragment = OrderFragment()
         val bundle = Bundle()
         bundle.putInt(OrderConstant.KEY_ORDER_STATUS,position)
         fragment.arguments = bundle
